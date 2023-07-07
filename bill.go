de89603448ec96393e06296c86ecc23d96a12f19
soulaimaneyh <soulaimaneyh07@gmail.com>
2023-07-07 02:53:02 -0400

    string helpers & receiver functions

--- bill.go
+++ bill.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 type bill struct {
 	name string
 	menu map[string]float32
@@ -14,4 +19,31 @@ func storeBill(name string, menu map[string]float32, tip float32) bill {
 		menu: menu,
 		tip: tip,
 	}
+}
+
+// string helpers
+func ucwords(str string) string {
+	if len(str) == 0 {
+		return str
+	}
+	firstLetter := strings.ToUpper(string(str[0]))
+	restOfStr := str[1:]
+	return firstLetter + restOfStr
+}
+
+// Receiver functions
+func (b bill) format() string {
+	fs := "Bill breakdown:\n"
+	var total float32 = 0
+
+	// list items
+	for k, v := range b.menu {
+		fs += fmt.Sprintf("%-25v ...$%0.2f\n", ucwords(k)+":", v)
+		total += v
+	}
+
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+
+	return fs
 }
