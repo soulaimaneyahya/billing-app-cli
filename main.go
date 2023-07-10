004b868a937eb1f1ae05fe29f6a13540d8554af7
soulaimaneyh <soulaimaneyh07@gmail.com>
2023-07-10 07:14:27 -0400

    fixing

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"strings"
 )
 
-func getInput(s string, r *bufio.Reader) (string, error) {
-	fmt.Print(s)
-	name, err := r.ReadString('\n')
+func getInput(propmt string, r *bufio.Reader) (string, error) {
+	fmt.Print(propmt)
+	input, err := r.ReadString('\n')
 
-	return strings.TrimSpace(name), err
+	return strings.TrimSpace(input), err
 }
 
 func createBill() bill {
@@ -20,6 +20,7 @@ func createBill() bill {
 	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
+
 	fmt.Printf("Created the bill - %v \n\n", b.name)
 
 	return b
@@ -29,6 +30,7 @@ func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 
 	opt, _ := getInput("Choose option (a -add item, s - save bill, t - add tip): ", reader)
+
 	fmt.Println(opt)
 }
 
